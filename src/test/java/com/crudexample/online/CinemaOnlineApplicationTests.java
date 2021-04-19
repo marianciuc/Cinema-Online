deef94f2145c4f405d0b3d3841e8a74d2599da35
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2021-04-19 07:30:39 +0300

    Add application main file

---
+++ src/test/java/com/crudexample/online/CinemaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudexample.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
