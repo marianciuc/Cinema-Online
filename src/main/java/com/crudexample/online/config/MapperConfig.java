e1c8cb4262b34840ff8fdc3be6e9372095c2041c
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2021-05-02 15:57:51 +0300

    feat: Added MapperConfig.java

---
+++ src/main/java/com/crudexample/online/config/MapperConfig.java
@@ -0,0 +1,28 @@
+package com.crudexample.online.config;
+
+
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+
+
+@AllArgsConstructor
+@org.springframework.context.annotation.Configuration
+public class MapperConfig {
+
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper
+                .getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+
+        return modelMapper;
+    }
+}
