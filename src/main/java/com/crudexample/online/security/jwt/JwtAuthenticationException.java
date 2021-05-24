765bdd6a3daaa666dc668ed9a82857fa12c57bad
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2021-05-24 11:03:38 +0300

    Revert "feat: nothing"
    This reverts commit 1b3489e6

--- src/main/java/com/crudexample/online/security/jwt/JwtAuthenticationException.java
+++ src/main/java/com/crudexample/online/security/jwt/JwtAuthenticationException.java
@@ -3,6 +3,11 @@ package com.crudexample.online.security.jwt;
 import org.springframework.security.core.AuthenticationException;
 
 public class JwtAuthenticationException extends AuthenticationException {
+
+    public JwtAuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
     public JwtAuthenticationException(String msg) {
         super(msg);
     }
